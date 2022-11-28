e584095d02832f867ee5de50feebd6bae39461ec
Pavel Ivanchik <wesp.pal@yandex.by>
2022-11-28 21:35:49 +0300

    Initial commit

---
+++ dmdev/src/main/java/com/dmdev/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package com.dmdev.util;
+
+import com.dmdev.converter.BirthdayConverter;
+import com.dmdev.entity.User;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = buildConfiguration();
+        configuration.configure();
+
+        return configuration.buildSessionFactory();
+    }
+
+    public static Configuration buildConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAttributeConverter(new BirthdayConverter());
+        configuration.registerTypeOverride(new JsonBinaryType());
+        return configuration;
+    }
+}
